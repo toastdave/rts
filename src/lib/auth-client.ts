2fe2a0f9ad4c991aafc4b780993b90fe0caeb875
DavidPierce24 <pie2ce@gmail.com>
2025-07-31 00:51:05 -0400

    add sharp for astro images

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,2 +1,12 @@
 import { createAuthClient } from "better-auth/react"
-export const authClient =  createAuthClient()
+export const authClient = createAuthClient({
+    baseURL: import.meta.env.PUBLIC_APP_URL,
+
+})
+
+export const {
+    signIn,
+    signOut,
+    signUp,
+    useSession
+} = authClient;
